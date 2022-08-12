2c56340d35f4fb05ac1b1644c8d401260dcea7f7
jefersonlopesl <jefersonlopesluis@duck.com>
2022-08-12 18:16:35 -0300

    corri alguns erros

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Title from './src/components/Title/'
-import Main from  './src/components/Main'
+import Main from  './src/components/Main/'
+
 export default function App() {
   return (
     <View style={styles.container}>
